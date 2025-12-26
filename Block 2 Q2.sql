1fcb331c6937df206583c7eb558df54694a4401e
richardademola123 <richardademola123@gmail.com>
2025-12-26 18:50:33 +0000

    Add files via upload

---
+++ Block 2 Q2.sql
@@ -0,0 +1,53 @@
+WITH field_flags AS (
+  SELECT
+    Country,
+    RecordID,
+    Datasource,
+    Field,
+    MatchStatusID,
+    CASE WHEN MatchStatusID = 2 THEN 1 ELSE 0 END AS is_match,
+    CASE WHEN MatchStatusID = 3 THEN 1 ELSE 0 END AS is_nomatch
+  FROM `vibrant-sound-459613-a3.trulioo.Field_table `
+),
+
+per_ds AS (
+  SELECT
+    Country,
+    RecordID,
+    Datasource,
+
+    
+    CASE 
+      WHEN SUM(CASE WHEN Field = 'FirstInitial' AND is_match = 1 THEN 1 ELSE 0 END) > 0
+       AND SUM(CASE WHEN Field = 'LastName'  AND is_match = 1 THEN 1 ELSE 0 END) > 0
+      THEN 1 ELSE 0
+    END AS name_match,
+
+    CASE 
+      WHEN SUM(CASE WHEN Field = 'DayOfBirth'   AND is_match = 1 THEN 1 ELSE 0 END) > 0
+       AND SUM(CASE WHEN Field = 'MonthOfBirth' AND is_match = 1 THEN 1 ELSE 0 END) > 0
+       AND SUM(CASE WHEN Field = 'YearOfBirth'  AND is_match = 1 THEN 1 ELSE 0 END) > 0
+      THEN 1 ELSE 0
+    END AS dob_match,
+
+    CASE 
+      WHEN SUM(CASE WHEN Field = 'StreetName'   AND is_match = 1 THEN 1 ELSE 0 END) > 0
+       AND SUM(CASE WHEN Field = 'HouseNumber'  AND is_match = 1 THEN 1 ELSE 0 END) > 0
+       AND (
+             SUM(CASE WHEN Field = 'PostalCode' AND is_match = 1 THEN 1 ELSE 0 END) > 0
+          OR SUM(CASE WHEN Field = 'City'       AND is_match = 1 THEN 1 ELSE 0 END) > 0
+       )
+       AND SUM(CASE WHEN Field = 'UnitNumber' AND is_nomatch = 1 THEN 1 ELSE 0 END) = 0
+      THEN 1 ELSE 0
+    END AS address_match
+
+  FROM field_flags
+  GROUP BY
+    Country,
+    RecordID,
+    Datasource
+)
+
+SELECT *
+FROM per_ds
+LIMIT 20;
